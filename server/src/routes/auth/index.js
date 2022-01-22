6fd0f5c968287602d9ac9a3d31eb61d11d5d6a4e
Eren Çifci <eren.cifci04@gmail.com>
2022-01-22 23:16:31 +0300

    register validation

--- server/src/routes/auth/index.js
+++ server/src/routes/auth/index.js
@@ -1,15 +1,49 @@
-import Boom from "boom";
 import express from "express";
+import Boom from "boom";
+import bcrypt from "bcryptjs";
+
+import Hasura from "../../clients/hasura";
+import { INSERT_USER_MUTATION, IS_EXISTS_USER } from "./queries";
+
+import { registerSchema } from "./validations";
+
 const router = express.Router();
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
     const input = req.body.input.data;
 
-    if (!input.email || !input.password) {
-        return next(Boom.badRequest("Email and password required"));
+    input.email = input.email.toLowerCase();
+
+    const { error } = registerSchema.validate(input);
+    if (error) {
+        return next(Boom.badRequest(error.details[0].message));
     }
 
-    res.json({ accessToken: "accessToken" });
+    try {
+        const isExistsUser = await Hasura.request(IS_EXISTS_USER, {
+            email: input.email,
+        });
+
+        if (isExistsUser.users.length > 0) {
+            throw Boom.conflict(`User already exists (${input.email})`);
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(input.password, salt);
+
+        const user = await Hasura.request(INSERT_USER_MUTATION, {
+            input: {
+                ...input,
+                password: hash,
+            },
+        });
+
+        res.json({ accessToken: "accessToken" });
+    } catch (err) {
+        return next(Boom.badRequest(err));
+    }
+
+    // res.json({ accessToken: "accessToken" });
 });
 
 export default router;
