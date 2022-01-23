721fdfc511d233c7a1ce8b88eab5810d7d2f2ead
Eren Çifci <eren.cifci04@gmail.com>
2022-01-23 03:43:17 +0300

    meeting reminder

--- server/src/routes/webhooks/index.js
+++ server/src/routes/webhooks/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import moment from "moment";
+import axios from "axios";
 import { createTransport } from "nodemailer";
 
 import Hasura from "../../clients/hasura";
-import { GET_MEETING_PARTICIPANTS } from "./queries";
+import {
+    GET_MEETING_PARTICIPANTS,
+    GET_MEETING_PARTICIPANTS_REMINDER,
+} from "./queries";
 
 const router = express.Router();
 
@@ -31,12 +36,68 @@ router.post("/meeting_created", async (req, res, next) => {
         .map(({ user }) => user.email)
         .toString();
 
+    const schedule_event = {
+        type: "create_scheduled_event",
+        args: {
+            webhook: "{{ACTION_BASE_ENDPOINT}}/webhooks/meeting_reminder",
+            schedule_at: moment(meeting.meeting_date).subtract(2, "minutes"),
+            payload: {
+                meeting_id: meeting.id,
+            },
+        },
+    };
+
+    const add_event = await axios("http://localhost:8080/v1/query", {
+        method: "POST",
+        data: JSON.stringify(schedule_event),
+        headers: {
+            "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+        },
+    });
+
+    const event_data = add_event.data;
+
+    console.log(event_data);
+
     const mailOptions = {
         from: process.env.GMAIL_USER,
         to: participants,
         subject: `${fullName} sizi bir görüşmeye davet etti`,
         text: `${fullName} sizi '${title}' adlı görüşmeye davet etti.
 Görüşme Tarihi: ${meeting.meeting_date}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return next(error);
+        }
+        return res.json({ info });
+    });
+});
+
+router.post("/meeting_reminder", async (req, res, next) => {
+    const { meeting_id } = req.body.payload;
+    console.log("Meeting reminder. meeting id:", meeting_id);
+
+    const { meetings_by_pk } = await Hasura.request(
+        GET_MEETING_PARTICIPANTS_REMINDER,
+        {
+            id: meeting_id,
+        }
+    );
+
+    const title = meetings_by_pk.title;
+    const { email } = meetings_by_pk.user;
+    const participants = meetings_by_pk.participants.map(
+        ({ user }) => user.email
+    );
+    participants.push(email);
+
+    const mailOptions = {
+        from: process.env.GMAIL_USER,
+        to: participants.toString(),
+        subject: `'${title}' başlıklı görüşmeniz birazdan başlayacak`,
+        text: `'${title}' başlıklı görüşmeniz birazdan başlayacak. Katılmak için aşağıdaki bağlantıyı kullanın.`,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
--- server/src/routes/webhooks/queries.js
+++ server/src/routes/webhooks/queries.js
@@ -1,10 +1,29 @@
 export const GET_MEETING_PARTICIPANTS = `
     query getParticipants($id: Int!) {
         meetings_by_pk(id: $id) {
+            title
             user {
+                email
                 fullName
             }
             participants {
+                user {
+                    email
+                }
+            }
+        }
+    }
+`;
+
+export const GET_MEETING_PARTICIPANTS_REMINDER = `
+    query getParticipants($id: Int!) {
+        meetings_by_pk(id: $id) {
+            title
+            user {
+                email
+                fullName
+            }
+            participants(where: { is_approved: { _eq: true } }) {
                 user {
                     email
                 }
